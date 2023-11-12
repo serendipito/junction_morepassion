2637a997e206f54d3453724af069b0a80d2726d1
Linas <linas.vastakas@fluidic.com>
2023-11-12 04:01:38 +0200

    Adding more videos and data

--- src/SongVideos.js
+++ src/SongVideos.js
@@ -1,16 +1,5 @@
 const SongVideos = [
-    { label: '21 Reasons', media: 'final_clip8.mp4' },
     { label: 'Baby Don\'t Hurt Me', media: 'final_clip9.mp4' },
-    { label: 'Bones', media: 'final_clip8.mp4' },
-    { label: 'Call It Love', media: 'final_clip8.mp4' },
-    { label: 'Car Keys', media: 'final_clip8.mp4' },
-    { label: 'Friday', media: 'final_clip8.mp4' },
-    { label: 'More Passion', media: '14.mov' },
-    { label: 'Sweetest Pie', media: '14.mov' },
-    { label: 'Thought It Was a Drought', media: '14.mp4' },
-    { label: 'Wake My Soul', media: '14.mp4' },
-
-  ];
+    {label:'Whistle',media:'whistle_final.mp4'},{label:'Bones',media:'bones_final.mpf'},{label:'All 4 Nothing', media: 'all4n.mp4'}]
   
-
 export default SongVideos;
