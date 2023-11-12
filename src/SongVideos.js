a733e27e083e306badc287d03121c00ef472976c
Linas <linas.vastakas@fluidic.com>
2023-11-12 04:06:27 +0200

    Adding backend and video files

--- src/SongVideos.js
+++ src/SongVideos.js
@@ -1,5 +1,5 @@
 const SongVideos = [
-    { label: 'Baby Don\'t Hurt Me', media: 'final_clip9.mp4' },
-    {label:'Whistle',media:'whistle_final.mp4'},{label:'Bones',media:'bones_final.mpf'},{label:'All 4 Nothing', media: 'all4n.mp4'}]
+    { label: 'Baby Don\'t Hurt Me', media: 'fe89.mov' },
+    {label:'Whistle',media:'whistle_final.mp4'},{label:'Bones',media:'bones_final.mp4'},{label:'All 4 Nothing', media: 'all4n.mp4'}]
   
 export default SongVideos;
